08d7aae7b9054647b8a1190f83fa02e77f4965a7
Stanislav Roslavtsev <jalchinn@yandex.ru>
2023-08-05 12:23:34 +0300

    Update airflow_test.py

--- airflow_test.py
+++ airflow_test.py
@@ -5,17 +5,20 @@ from datetime import datetime, timedelta
 from airflow import DAG
 
 
-@task(task_id='test_airflow', retries=3)
-def airflow_test(chat_id):
-    response = r.get('https://www.google.com').text
-
+@task(task_id="send_test_telegram_message", retries=3)
+def send_message(chat_id, text):
     r.post(
-        f'https://api.telegram.org/bot6380116078:AAGp5FkW_cM4IMS8Ft9tewztECBTz-c-_EM/sendMessage',
+        f"https://api.telegram.org/bot6380116078:AAGp5FkW_cM4IMS8Ft9tewztECBTz-c-_EM/sendMessage",
         json={
             "chat_id": chat_id,
-            "text": response,
+            "text": text,
         }
     )
+
+
+@task(task_id='test_airflow', retries=3)
+def airflow_test():
+    response = r.get('https://www.google.com').text
 
     return response
 
@@ -32,7 +35,8 @@ def airflow_test(chat_id):
     }
 )
 def airflow_testing():
-    balance = airflow_test(818677727)
+    response = airflow_test(818677727)
+    send_message(818677727, str(response))
 
 
 airflow_tests = airflow_testing()
