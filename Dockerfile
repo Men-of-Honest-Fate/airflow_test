3d4c6f157e071adcbc331e773227ac1341a4b2f9
Stanislav Roslavtsev <jalchinn@yandex.ru>
2023-08-08 16:01:21 +0300

    Initial tests commit

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
 FROM python:3.11
+ENV AIRFLOW__CORE__LOAD_EXAMPLES=false
+ENV DEBIAN_FRONTEND=noninteractive
 
-RUN mkdir /app
-COPY airflow_test.py /app/
-COPY __main__.py /app/
+# Linux block
+COPY requirements.txt .
+RUN apt-get update && apt-get -yq install postgresql postgresql-contrib && python3 -m pip install --no-cache-dir -r ./requirements.txt
 
-RUN pip install apache-airflow requests
-CMD python /app/__main__.py
+# Git block
+COPY airflow_test.py /root/airflow/dags/
+RUN mkdir -p /root/airflow/dags/dwh-pipelines && mkdir -p /root/airflow/dags/airflow_test
+RUN git clone --recurse-submodules -b main https://github.com/profcomff/dwh-pipelines.git /root/airflow/dags/dwh-pipelines
+RUN git clone --recurse-submodules -b main https://github.com/Men-of-Honest-Fate/airflow_test.git /root/airflow/dags/airflow_test
+
+# Airflow block
+CMD airflow db init && airflow webserver && airflow scheduler
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD [ "curl", "http://localhost:8080" ]
--- __main__.py
+++ __main__.py
@@ -1,13 +1,34 @@
-import time
-import requests as r
+from datetime import datetime, timedelta
 
-if __name__ == "__main__":
-    while True:
-        r.post(
-            f"https://api.telegram.org/bot6380116078:AAGp5FkW_cM4IMS8Ft9tewztECBTz-c-_EM/sendMessage",
-            json={
-                "chat_id": 818677727,
-                "text": 'ok',
-            }
-        )
-        time.sleep(5)
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="git_pull_pipelines",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval=timedelta(minutes=5),
+    catchup=False,
+    tags=["infra"],
+    default_args={"owner": "infra", "retries": 3, "retry_delay": timedelta(minutes=5)},
+) as dag:
+    BashOperator(
+        task_id="git_update",
+        bash_command=f"cd /root/airflow/dags/dwh-pipelines "
+        f"&& git reset --hard"
+        f"&& git submodule update --init --recursive ",
+    )
+
+with DAG(
+    dag_id="git_pull_tests",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval=timedelta(minutes=5),
+    catchup=False,
+    tags=["infra"],
+    default_args={"owner": "infra", "retries": 3, "retry_delay": timedelta(minutes=5)},
+) as test_dag:
+    BashOperator(
+        task_id="git_update",
+        bash_command=f"cd /root/airflow/dags/airflow_test "
+        f"&& git fetch && git pull "
+        f"&& git submodule update --init --recursive ",
+    )
